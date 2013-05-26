d9bbcea63335822570332ffca7f3a702e30ec776
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2013-05-26 18:30:37 +0200

    Ex11 + progress waiting loop.

--- ex10.py
+++ ex10.py
@@ -2,12 +2,12 @@ tabbyCat = "\tI'm tabbed in."
 persianCat = "I'm split\non a line."
 backslashCat = "I'm \\ a \\ cat."
 
-fatCat = """
+fatCat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print tabbyCat
 print persianCat
