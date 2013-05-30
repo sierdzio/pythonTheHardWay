1eef12da62a8db491376559724e2c1497b1bd111
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2013-05-30 16:17:20 +0200

    Add some nice bonus lines :D

--- ex15.py
+++ ex15.py
@@ -6,6 +6,7 @@ txt = open(filename)
 
 print "Here's your file %s:" % filename
 print txt.read()
+txt.close()
 
 print "Type the filename again:"
 fileAgain = raw_input('> ')
@@ -13,5 +14,6 @@ fileAgain = raw_input('> ')
 if filename == fileAgain:
   txtAgain = open(fileAgain)
   print txtAgain.read()
+  txtAgain.close()
 else:
   print "Filenames don't match: %s, %s." % (filename, fileAgain)
